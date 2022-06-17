e8403fc73ca885c3958bd5ca6c7b6d7f5da19ac3
shuamp <castel.davila@gmail.com>
2022-06-17 19:50:45 +0100

    search page created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Footer from '../components/footer'
 import NavBar from '../components/navbar'
 import styles from '../styles/Home.module.css'
 
@@ -14,6 +15,7 @@ const Home: NextPage = () => {
       </Head>
       <NavBar/>
       <h1>Bread of The Day</h1>
+      <Footer/>
 
      
     </div>
---
+++ pages/search.tsx
@@ -0,0 +1,14 @@
+import Footer from "../components/footer"
+import NavBar from "../components/navbar"
+
+
+const Search = () => {
+  return (
+    <div>
+        <NavBar/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Search
